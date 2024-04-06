1e2f1f1cb73b4c6c899c9ecff667baa7938f4c9b
Shreyansh Raval <104890198+ShreyanshRaval1167@users.noreply.github.com>
2024-04-06 12:17:56 +0530

    g

--- js/script2.js
+++ js/script2.js
@@ -1,15 +1,25 @@
+const sidebar = document.getElementById('sidebar');
+const sidebarButtons = document.querySelectorAll('.navbar-1');
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if (bar) {
-  bar.addEventListener('click',() =>{
+  bar.addEventListener('click', () => {
     nav.classList.add('active');
-  })
+  });
 }
 
 if (close) {
-  close.addEventListener('click',() =>{
+  close.addEventListener('click', () => {
     nav.classList.remove('active');
-  })
+  });
+}
+
+if (sidebarButtons) {
+  sidebarButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      nav.classList.remove('active');
+    });
+  });
 }
